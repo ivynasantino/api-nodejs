4653e75ba88c8e1a8f2af96628d81233ea9d6af2
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-02-13 20:49:02 -0300

    retira a demonstração do email do cliente e corrige envio de mensagem no registro de emails iguais

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,6 +11,8 @@ router.post('/register', async(req, res) => {
 
         const user = await User.create(req.body);
 
+        user.password = undefined;
+
         return res.send({ user });
 
     } catch (err) {
