90630b54954d73f4b399ff8e150d26c77f906893
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-02-13 19:53:35 -0300

    adiciona rota inicial

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
+
+app.get('/', (req, res) => {
+    res.send('OK');
+});
+
+app.listen(3000);
