4d9e0bde248cc89a98df8471d9ed4ca3c4002e6f
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-02-13 20:32:29 -0300

    adiciona require do auth

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('OK');
-});
-
+require('./controllers/authController')(app);
 app.listen(3000);
